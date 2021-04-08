e8330c083f75a94ef5520de475df55c1981b14cf
m3v113 <76668066+m3v113@users.noreply.github.com>
2021-04-08 10:47:52 -0700

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,32 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("block.png");
+      this.visibility = 255;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+      push();
+      if(this.body.speed < 3) {
+        //rectMode
+        
+       }
+    
+       else {
+        World.remove(world, this.body);
+        this.visibility = this.visibility - 5;
+        tint(235, this.visibility)
+        image(this.image, this.body.position.x, this.body.position.y, 30,40);
+       }
+      pop();
+    }
+}
